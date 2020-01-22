5b2b6e973932f29ad5aa04c6932a5144efd30b11
Andy Camann <acamann@gmail.com>
2020-01-21 18:54:06 -0600

    display simple table of race data

--- pace-data.js
+++ pace-data.js
@@ -6,6 +6,7 @@ function loadRaceData() {
     .then(data => {
         raceData = data.races;   
         displayPaceGraph();
+        displayRaceTable(raceData, ['date', 'raceName', 'distanceMiles', 'time', 'pace']);
         createCheckBoxes();
     })
 }
@@ -92,6 +93,38 @@ function createCheckBoxes() {
             d3.selectAll("circle." + this.name).style("opacity", newOpacity);
             d3.selectAll("text." + this.name).style("opacity", newOpacity);
         });
+}
+
+function displayRaceTable(data, columns) {
+    var table = d3.select('#race-table-container').append('table')
+    var thead = table.append('thead')
+    var	tbody = table.append('tbody');
+
+    // append the header row
+    thead.append('tr')
+      .selectAll('th')
+      .data(columns).enter()
+      .append('th')
+        .text(function (column) { return column; });
+
+    // create a row for each object in the data
+    var rows = tbody.selectAll('tr')
+      .data(data)
+      .enter()
+      .append('tr');
+
+    // create a cell in each row for each column
+    var cells = rows.selectAll('td')
+      .data(function (row) {
+        return columns.map(function (column) {
+          return {column: column, value: row[column]};
+        });
+      })
+      .enter()
+      .append('td')
+        .text(function (d) { return d.value; });
+
+  return table;
 }
 
 
